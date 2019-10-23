2bfed663f08fceab5069b66f610f71d079e24ac4
haroomiokum <haroomiokum@gmail.com>
2019-10-23 05:16:51 +0000

    Some fix

---
+++ kadai-1/index.js
@@ -0,0 +1,13 @@
+console.log(name)
+const name = 'Pochi'
+const age=10
+//コメント
+const name = 'Tama'
+const age=10
+console.log(name)
+console.log(age)
+let name = 'Pochi'
+let number = 10
+number = number + 1
+console.log(number)
+const phrase1 = "I'm a cat."
