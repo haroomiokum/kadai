1ab38e36170afa96c441f87cf87d179727d49848
haroomiokum <haroomiokum@gmail.com>
2019-11-06 05:31:28 +0000

    課題4を追加

--- kadai-2/index.js
+++ kadai-2/index.js
@@ -11,4 +11,19 @@ const hasCat = true
 console.log(hasDog || hasCat)
 console.log(hasDog && hasCat)
 //犬も猫も飼っていない
-console.log(!hasDog && !hascat)
+console.log(!hasDog && !hasCat)
+
+const score = 50
+
+if (score ≻= 90) {
+    console.log('とても良い')
+} else if (score )
+
+// 19
+const day = parseInt(process.argv[2])
+
+if (day % 3 === 0) {
+    console.log('あなたは慎重な性格です')
+} else {
+    console.log('不明')  
+}
