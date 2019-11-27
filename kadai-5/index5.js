0b0e87ce3a0df023e6702d45dda10e35b693b72c
haroomiokum <haroomiokum@gmail.com>
2019-11-27 04:57:42 +0000

    かだい６提出

--- kadai-5/index5.js
+++ kadai-5/index5.js
@@ -9,4 +9,18 @@ const profile = {
 
 // console.log(profile.firstName) // 'Mio'
 console.log(profile["first-name"]) // 'Mio'
-const key1 = 
+const keys = object.keys(profile);
+
+for (let i = 0; i < keys.length; i = i + 1) {
+    const key = keys[i];
+    console.log(profile[key]);
+    const value = profile[key];
+    const text = "私の" + key + "は、"　+ value + "です。"
+    console.log(text);
+}
+
+for (let key of keys) {
+    const value = profile[key];
+    const text = "私の" + key + "は、" + value + "です。";
+    console.log(text);
+}
